fd0f6aa6f2e1110d2586d2eedaf1d6810fcafc02
Yauheni <Litvinenko.man@gmail.com>
2025-02-09 15:38:52 +0100

    Added another Interface for UI

---
+++ Assets/_Scripts/Interfaces/ICircleGameplayPresenter.cs
@@ -0,0 +1,8 @@
+namespace _Scripts.Interfaces
+{
+    public interface ICircleGameplayPresenter
+    {
+        void VisualizePlayerScore(int score);
+        void VisualizePlayerDistance(float distance);
+    }
+}
